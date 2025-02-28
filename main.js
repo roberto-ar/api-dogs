56b1393725f2811422db5fd150b36834b342fcd1
roberto_ar <robertolejandroatilanoarias@gmail.com>
2025-02-27 20:49:33 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let menu = document.getElementById("razas");
+let images = 
+
+fetch("https://dog.ceo/api/breeds/list/all") // URL correcta
+    .then(response => response.json())
+    .then(data => {
+        menu.innerHTML = "";
+        let razas = Object.keys(data.message);
+        razas.forEach(raza => {
+            menu.innerHTML += `<option value="${raza}">${raza}</option>`;
+        });
+    });
+
+document.getElementById("razas").addEventListener("change", () =>{
+    fetch(`https://dog.ceo/api/breed/${menu.value}/images`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("imagenes").innerHTML = `
+        <img src="${data.message[0]}" alt="${menu.value}" class="rounded">
+        <img src="${data.message[1]}" alt="${menu.value}" class="rounded">
+        <img src="${data.message[2]}" alt="${menu.value}" class="rounded">
+        `
+    })    
+})
+
